e0336ecdb8b82693f47a70aa6ea356eb12ca59dd
Andres2D <andres2d1997.1206@gmail.com>
2023-07-31 12:07:50 -0500

    Create an use a new component

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,71 +1,29 @@
 <script>
+import Counter from './components/Counter.vue';
+
 export default {
+  components: {
+    Counter
+  },
   data() {
     return {
-      count: 0,
-      counterTitle: 'Counter Standard',
-      incrementAmount: 7,
       message: 'Hello it works',
       listOfNumbers: [1, 2, 3, 4, 5],
     };
   },
-  computed: {
-    displayTitle() {
-      if (this.count > 21) {
-        return 'Counter Standard - Very Long';
-      } else {
-        return 'Counter Standard';
-      }
-    },
-    optimizedIncrementAmount() {
-      return this.displayTitle.length * this.incrementAmount;
-    },
-  },
-  methods: {
-    incrementCount(newAmount, event) {
-      // console.log(newAmount);
-      // console.log(event);
-      this.count += this.optimizedIncrementAmount;
-    },
-    // changeIncrementAmount(event) {
-    //   console.log(event);
-    //   this.incrementAmount = +event.target.value;
-    // }
-  },
-  watch: {
-    count(newValue) {
-      if (newValue > 20) {
-        this.counterTitle += ' Very long';
-      }
-    },
-  },
-}
+};
 </script>
 
 <template>
+  <Counter />
   <h1>test</h1>
-  <div id="app">
-    <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
-    <p v-else>Odd: {{ message }}</p>
-    <ul>
-      <li v-for="(number, index) in listOfNumbers" :key="`item-${index}`">{{ number }}</li>
-    </ul>
+  <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
+  <p v-else>Odd: {{ message }}</p>
+  <ul>
+    <li v-for="(number, index) in listOfNumbers" :key="`item-${index}`">
+      {{ number }}
+    </li>
+  </ul>
 
-    <hr />
-
-    <h1>{{ displayTitle }}</h1>
-    <p>{{ count }}</p>
-    <p>{{ optimizedIncrementAmount }}</p>
-    <button @click="incrementCount(10, $event)">Increment Count</button>
-    <!-- v-on:click="incrementCount(10, $event)"  -->
-    <!-- v-on:click="(event) => incrementCount(10, event)"  -->
-    <!-- v-on:click="incrementCount(10)"  -->
-
-    <div>
-      <label for="incrementamount">Increment by:</label>
-      <input type="number" v-model="incrementAmount" />
-      <!-- v-bind:value="incrementAmount" 
-          v-on:input="changeIncrementAmount" -->
-    </div>
-  </div>
+  <hr />
 </template>
---
+++ sandbox/src/components/Counter.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  data: () => ({
+    count: 0,
+    counterTitle: 'Counter Standard',
+    incrementAmount: 7,
+  }),
+  computed: {
+    displayTitle() {
+      if (this.count > 21) {
+        return 'Counter Standard - Very Long';
+      } else {
+        return 'Counter Standard';
+      }
+    },
+    optimizedIncrementAmount() {
+      return this.displayTitle.length * this.incrementAmount;
+    },
+  },
+  methods: {
+    incrementCount(newAmount, event) {
+      // console.log(newAmount);
+      // console.log(event);
+      this.count += this.optimizedIncrementAmount;
+    },
+    // changeIncrementAmount(event) {
+    //   console.log(event);
+    //   this.incrementAmount = +event.target.value;
+    // }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ displayTitle }}</h1>
+  <p>{{ count }}</p>
+  <p>{{ optimizedIncrementAmount }}</p>
+  <button @click="incrementCount(10, $event)">Increment Count</button>
+  <!-- v-on:click="incrementCount(10, $event)"  -->
+  <!-- v-on:click="(event) => incrementCount(10, event)"  -->
+  <!-- v-on:click="incrementCount(10)"  -->
+
+  <div>
+    <label for="incrementamount">Increment by:</label>
+    <input type="number" v-model="incrementAmount" />
+    <!-- v-bind:value="incrementAmount" 
+          v-on:input="changeIncrementAmount" -->
+  </div>
+</template>
