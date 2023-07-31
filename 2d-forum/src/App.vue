0706eb77c9756e092feb0992fd8b1f765b2d3a04
Andres2D <andres2d1997.1206@gmail.com>
2023-07-31 11:41:50 -0500

    Migrate cdn example to 2d project

---
+++ 2d-forum/src/App.vue
@@ -0,0 +1,158 @@
+<script>
+  export default {
+    data() {
+      return {
+        newCharacter: {
+          name: '',
+          favorite: false,
+          state: 'Alive',
+          stars: 0
+        },
+        characters: [
+          {
+            id: 1,
+            name: 'Barry',
+            state: 'Death',
+            favorite: false,
+            stars: 5
+          },
+          {
+            id: 2,
+            name: 'Noho Hank',
+            state: 'Death',
+            favorite: false,
+            stars: 4
+          },
+          {
+            id: 3,
+            name: 'The raven',
+            state: 'Alive',
+            favorite: false,
+            stars: 4
+          },
+          {
+            id: 4,
+            name: 'Gene',
+            state: 'Alive',
+            favorite: false,
+            stars: 4
+          }
+        ]
+      }
+    },
+    computed: {
+      deathCharacterPercentage() {
+        return `${
+          (this.characters.filter((char) => char.state === 'Death').length / this.characters.length) *
+          100
+        }%`
+      },
+      aliveCharacterPercentage() {
+        return `${
+          (this.characters.filter((char) => char.state === 'Alive').length / this.characters.length) *
+          100
+        }%`
+      },
+      averageStars() {
+        return Math.floor(
+          this.characters.map((char) => char.stars).reduce((a, b) => a + b) / this.characters.length
+        )
+      }
+    },
+    methods: {
+      addRemoveFavorite(id) {
+        this.characters[this.characters.findIndex((char) => char.id === id)].favorite =
+          !this.characters[this.characters.findIndex((char) => char.id === id)].favorite
+      },
+      addNewCharacter(event) {
+        event.preventDefault()
+        const newCharacterObject = {
+          ...this.newCharacter,
+          id: this.characters[this.characters.length - 1] + 1
+        }
+        this.characters.push(newCharacterObject)
+        this.newCharacter = {
+          name: '',
+          favorite: false,
+          state: 'Alive'
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <p v-if="characters.length === 0">There are no characters</p>
+  <ul v-else>
+    <li v-for="(character, index) in characters.filter((char) => char.favorite)" 
+      :key="`character-${index}`">
+      {{ index + 1 }}] {{ character.name }} ({{ character.state }})
+      <button @click="addRemoveFavorite(character.id)">⬇️</button>
+    </li>
+
+    <hr />
+
+    <li v-for="(character, index) in characters.filter((char) => !char.favorite)"
+      :key="`character-${index}`">
+      {{ index + 1 }}] {{ character.name }} ({{ character.state }})
+      <button @click="addRemoveFavorite(character.id)">🔥</button>
+    </li>
+
+    <hr />
+
+    <form>
+      <label>Character name</label>
+      <input v-model="newCharacter.name" type="text" />
+      <br />
+      <label>State</label>
+      <select v-model="newCharacter.state">
+        <option value="Death">Death</option>
+        <option value="Alive">Alive</option>
+      </select>
+      <br />
+      <label>Stars</label>
+      <input v-model="newCharacter.stars" type="number" />
+      <br />
+      <label>Favorite</label>
+      <input v-model="newCharacter.favorite" type="checkbox" />
+      <br />
+      <button type="button" @click="addNewCharacter">Save</button>
+    </form>
+
+    <hr />
+
+    <h2>Death characters: {{ deathCharacterPercentage }}</h2>
+    <h2>Alive characters: {{ aliveCharacterPercentage }}</h2>
+
+    <h2>Average characters stars: {{ averageStars }}</h2>
+  </ul>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
