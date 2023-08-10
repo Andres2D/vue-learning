f674e01aabbdb1e29a15701bbdc34c99e731e32c
Andres2D <andres2d1997.1206@gmail.com>
2023-08-10 17:02:07 -0500

    Migrate full code to single component

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,68 +1,6 @@
 <script>
-import BaseCounter from './components/BaseCounter.vue';
-import UserCard from './components/UserCard.vue';
-import BaseButton from './components/BaseButton.vue';
-import BaseLayout from './components/BaseLayout.vue';
-
-export default {
-  components: {
-    BaseCounter,
-    UserCard,
-    BaseButton,
-    BaseLayout
-  },
-  data() {
-    return {
-      message: 'Hello it works',
-      listOfNumbers: [1, 2, 3, 4, 5],
-      userData: {
-        name: 'Ben',
-        framework: 'vue',
-        food: 'sushi',
-        numbers: [8, 10, 12]
-      }
-    };
-  },
-  computed: {
-    defineUserData() {
-      return {
-        name: this.userData.name,
-        food: this.userData.food
-      }
-    }
-  },
-  methods: {
-    changeName() {
-      this.userData.name = 'Charlie'
-    }
-  }
-};
 </script>
 
 <template>
-  <BaseLayout>
-    <template #sidebar>
-      Sidebar
-    </template>
-    <template #main> 
-      Main content
-    </template>
-    <template #footer> 
-      Footer content
-    </template>
-  </BaseLayout>
-  <BaseButton :left="true"></BaseButton>
-  <UserCard :user="defineUserData" @change-name="changeName"/>
-  <BaseCounter />
-  <!-- <base-counter /> -->
-  <h1>test</h1>
-  <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
-  <p v-else>Odd: {{ message }}</p>
-  <ul>
-    <li v-for="(number, index) in listOfNumbers" :key="`item-${index}`">
-      {{ number }}
-    </li>
-  </ul>
-
-  <hr />
+  <h1>New App</h1>
 </template>
