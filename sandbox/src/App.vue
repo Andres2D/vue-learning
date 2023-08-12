fe684efaf6125e7d2debd2fe5ab904b3ebdbe2b1
Andres2D <andres2d1997.1206@gmail.com>
2023-08-12 15:57:37 -0500

    Using Suspense component to fix rendering problem

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,28 +1,19 @@
 <script>
-// https://pokeapi.co/api/v2/pokemon/limit=151
+import Pokedex from './components/Pokedex.vue';
+
 export default {
-  async setup() {
-    const regionName = 'kanto';
-    const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-      .then(response => response.json());
-    
-    return {
-      regionName,
-      pokedex
-    }
-  },
-  data: () => ({
-    pokedex: [1,2,3]
-  }),
-  created() {
-    console.log(this.regionName)
-    console.log(this.pokedex);
+  components: {
+    Pokedex
   }
 }
 </script>
 
 <template>
   <h1>New App</h1>
-  <pre>{{  pokedex  }}</pre>
-  <button @click="fetchPokemon">Fetch pokemon</button>
+  <Suspense>
+    <Pokedex />
+    <template #fallback>
+      Fetching data...
+    </template>
+  </Suspense>
 </template>
---
+++ sandbox/src/components/Pokedex.vue
@@ -0,0 +1,27 @@
+<script>
+// https://pokeapi.co/api/v2/pokemon/limit=151
+export default {
+  async setup() {
+    const regionName = 'kanto';
+    const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json());
+    
+    return {
+      regionName,
+      pokedex
+    }
+  },
+  data: () => ({
+    pokedex: [1,2,3]
+  }),
+  created() {
+    console.log(this.regionName)
+    console.log(this.pokedex);
+  }
+}
+</script>
+
+<template>
+  <h2>{{ regionName }}</h2>
+  <pre>{{  pokedex  }}</pre>
+</template>
