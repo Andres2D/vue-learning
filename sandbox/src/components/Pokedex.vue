865fdf8444288124bffd780eac738d80d98619b1
Andres2D <andres2d1997.1206@gmail.com>
2023-08-12 22:01:43 -0500

    Using ref to add reactivity to setup variables

--- sandbox/src/components/Pokedex.vue
+++ sandbox/src/components/Pokedex.vue
@@ -1,27 +1,44 @@
 <script>
-// https://pokeapi.co/api/v2/pokemon/limit=151
+import { computed, ref } from 'vue';
+
 export default {
   async setup() {
-    const regionName = 'kanto';
+    const regionName = ref('Kanto');
+    console.log(regionName);
+    const regionNameAllCaps = computed(() => regionName.value.toUpperCase());
     const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
       .then(response => response.json());
     
     return {
       regionName,
-      pokedex
+      pokedex,
+      regionNameAllCaps
     }
   },
   data: () => ({
     pokedex: [1,2,3]
   }),
+  computed: {
+    regionNameLowerCase() {
+      return this.regionName.toLowerCase();
+    }
+  },
+  methods: {
+    changeRegionName() {
+      this.regionName = 'Hoenn';
+    }
+  },
   created() {
-    console.log(this.regionName)
-    console.log(this.pokedex);
+    // console.log(this.regionName)
+    // console.log(this.pokedex);
   }
 }
 </script>
 
 <template>
   <h2>{{ regionName }}</h2>
+  <h3>{{ regionNameAllCaps }}</h3>
+  <h3>{{ regionNameLowerCase }}</h3>
+  <button @click="changeRegionName">Change region name</button>
   <pre>{{  pokedex  }}</pre>
 </template>
