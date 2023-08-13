2521fff7df3631102c1cf00037045bc974ef010e
Andres2D <andres2d1997.1206@gmail.com>
2023-08-12 22:30:49 -0500

    Simplify component setup to use composition API

--- sandbox/src/components/Pokedex.vue
+++ sandbox/src/components/Pokedex.vue
@@ -1,51 +1,26 @@
-<script>
+<script setup>
+import BaseButton from './BaseButton.vue';
 import { computed, ref, reactive } from 'vue';
 
-export default {
-  async setup() {
-    const regionName = ref('Kanto');
+const regionName = ref('Kanto');
 
-    const state = reactive({
-      elementType: 'lighting'
-    });
+const state = reactive({
+  elementType: 'lighting'
+});
 
-    console.log(regionName);
-    const regionNameAllCaps = computed(() => state.elementType.toUpperCase());
-    const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-      .then(response => response.json());
-    
-    console.log(state);
-    
-    return {
-      regionName,
-      pokedex,
-      regionNameAllCaps
-    }
-  },
-  data: () => ({
-    pokedex: [1,2,3]
-  }),
-  computed: {
-    regionNameLowerCase() {
-      return this.regionName.toLowerCase();
-    }
-  },
-  methods: {
-    changeRegionName() {
-      this.regionName = 'Hoenn';
-    }
-  },
-  created() {
-    // console.log(this.regionName)
-    // console.log(this.pokedex);
-  }
-}
+const regionNameAllCaps = computed(() => state.elementType.toUpperCase());
+const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  .then(response => response.json());
+
+const changeRegionName = () => {
+  regionName.value = 'Hoenn';
+};
 </script>
 
 <template>
+  <BaseButton />
   <h2>{{ regionName }}</h2>
   <h3>{{ regionNameAllCaps }}</h3>
-  <h3>{{ regionNameLowerCase }}</h3>
   <button @click="changeRegionName">Change region name</button>
   <pre>{{  pokedex  }}</pre>
 </template>
