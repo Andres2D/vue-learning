c88ac2cca20a739f0fc7b2e067f9d22fe86e7e9a
Andres2D <andres2d1997.1206@gmail.com>
2023-08-15 14:54:19 -0500

    Add v bind classes

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,19 +1,31 @@
 <script setup>
 import BaseCounter from './components/BaseCounter.vue';
 import UserCard from './components/UserCard.vue';
+import { ref } from 'vue';
+
+const colorPreference = ref('black');
 
 </script>
 
 <template>
-  <BaseCounter />
-  <UserCard
-    :user="{name: 'Ben', food: 'sushi'}" 
-  />
+  <div class="wrapper">
+    <p>{{ colorPreference }}</p>
+    <input type="color" v-model="colorPreference" />
+    <BaseCounter />
+    <UserCard
+      :user="{name: 'Ben', food: 'sushi'}" 
+    />
+  </div>
+  
 </template>
 
 <style>
 html {
   background-color: papayawhip;
+}
+
+.wrapper {
+  background-color: v-bind('colorPreference');
 }
 
 button {
