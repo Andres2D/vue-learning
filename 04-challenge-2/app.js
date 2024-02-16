702e9e60ec76f5335ec3c1850777ed7925e99937
Andres2D <andres2d1997.1206@gmail.com>
2024-02-16 18:58:33 -0500

    Challenge 2 solution

--- 04-challenge-2/app.js
+++ 04-challenge-2/app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
   data() {
-    
+    return {
+      firstInput: '',
+      secondOutput: ''
+    }
   },
   methods: {
-
+    openAlert() {
+      alert('Hey buddy, I am an alert');
+    },
+    printInput(event) {
+      this.firstInput = event.target.value; 
+    },
+    printInputOnEnter(event) {
+      this.secondOutput = event.target.value; 
+    }
   }
 });
 
